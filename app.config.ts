943ca7b360791c6c2b9fae0566dad90366e7cda7
dev-rb <rbatra486@gmail.com>
2024-10-26 16:54:42 -0400

    Remove README content. Bump packages

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "@solidjs/start/config";
-import UnoCSS from "unocss/vite";
+import { defineConfig } from '@solidjs/start/config';
+import UnoCSS from 'unocss/vite';
 
 export default defineConfig({
   vite: {
-    plugins: [UnoCSS()]
-  }
+    plugins: [UnoCSS()],
+  },
 });
