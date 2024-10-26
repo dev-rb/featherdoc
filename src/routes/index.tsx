546929e8f2d691487a352b2107e303b68ed42607
dev-rb <rbatra486@gmail.com>
2024-10-26 12:25:24 -0400

    Split sidenav to separate components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,52 +1,51 @@
 import { A } from '@solidjs/router';
+import { VoidComponent } from 'solid-js';
+
+interface NavLinkProps {
+  href: string;
+  iconClass: string;
+}
+
+const NavLink: VoidComponent<NavLinkProps> = (props) => {
+  return (
+    <A
+      class="flex items-center justify-center p-2 rounded-lg text-slate-400"
+      href={props.href}
+      activeClass="bg-gray-700 text-white"
+      inactiveClass="hover:(bg-gray-800 text-gray-200)"
+    >
+      <i class={`${props.iconClass} text-2xl`} />
+    </A>
+  );
+};
+
+const Navigation = () => {
+  return (
+    <nav class="w-full h-full">
+      <ul class="w-full h-full flex flex-col items-center gap-6">
+        <li>
+          <NavLink href="/" iconClass="i-lucide-home" />
+        </li>
+        <li>
+          <NavLink href="/notebook" iconClass="i-lucide-notebook" />
+        </li>
+        <li>
+          <NavLink href="/threads" iconClass="i-lucide-message-square" />
+        </li>
+        <li class="mt-auto">
+          <NavLink href="/settings" iconClass="i-lucide-settings" />
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default function Home() {
   return (
     <div class="bg-gray-900 w-screen h-screen">
       <div class="w-full h-full grid grid-cols-[auto_1fr] grid-rows-1">
         <aside class="py-8 px-4 h-full border-r-2 border-r-gray-800">
-          <ul class="w-full h-full flex flex-col items-center gap-6">
-            <li>
-              <A
-                class="flex items-center justify-center p-2 rounded-lg text-slate-400"
-                href="/"
-                activeClass="bg-gray-700 text-white"
-                inactiveClass="hover:(bg-gray-800 text-gray-200)"
-              >
-                <i class="i-lucide-home text-2xl" />
-              </A>
-            </li>
-            <li>
-              <A
-                class="flex items-center justify-center p-2 rounded-lg text-slate-400"
-                href="/notebook"
-                activeClass="bg-gray-700 text-white"
-                inactiveClass="hover:(bg-gray-800 text-gray-200)"
-              >
-                <i class="i-lucide-notebook text-2xl" />
-              </A>
-            </li>
-            <li>
-              <A
-                class="flex items-center justify-center p-2 rounded-lg text-slate-400"
-                href="/threads"
-                activeClass="bg-gray-700 text-white"
-                inactiveClass="hover:(bg-gray-800 text-gray-200)"
-              >
-                <i class="i-lucide-message-square text-2xl" />
-              </A>
-            </li>
-            <li class="mt-auto">
-              <A
-                class="flex items-center justify-center p-2 rounded-lg text-slate-400"
-                href="/settings"
-                activeClass="bg-gray-700 text-white"
-                inactiveClass="hover:(bg-gray-800 text-gray-200)"
-              >
-                <i class="i-lucide-settings text-2xl" />
-              </A>
-            </li>
-          </ul>
+          <Navigation />
         </aside>
         <main class="w-full h-full grid grid-cols-[auto_2fr_1fr]">
           <aside class="bg-gray-700 min-w-md"></aside>
