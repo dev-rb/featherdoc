c278de5bb927fcc9ca3c8071e6388226f6d8eed5
dev-rb <me@devrb.dev>
2024-10-26 23:52:58 -0400

    misc: random dashboard ui

--- src/routes/(layout)/index.tsx
+++ src/routes/(layout)/index.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
-    <main class="w-full h-full grid grid-cols-12">
-      <div class="bg-muted min-w-md"></div>
-      <div class="bg-primary min-w-md"></div>
-      <div class="bg-destructive min-w-md"></div>
-      <div class="bg-muted min-w-md"></div>
-      <div class="bg-muted min-w-md"></div>
-      <div class="bg-muted min-w-md"></div>
-      <div class="bg-muted min-w-md"></div>
+    <main class="w-full h-full grid grid-cols-12 grid-rows-12 bg-secondary">
+      <div class="w-full h-full col-start-2 row-start-2 col-end-6 row-end-6 rounded-md border-1 border-neutral-600"></div>
+
+      <div class="w-full h-full col-start-8 row-start-2 col-end-12 row-end-8 rounded-md border-1 border-neutral-600"></div>
+
+      <div class="w-full h-full col-start-2 row-start-7 col-end-6 row-end-12 rounded-md border-1 border-neutral-600"></div>
+
+      <div class="w-full h-full col-start-7 row-start-9 col-end-12 row-end-12 rounded-md border-1 border-neutral-600"></div>
     </main>
   );
 }
