170fdd2841fce66b790dda242032f47a211e9e2f
dev-rb <rbatra486@gmail.com>
2024-11-03 14:18:06 -0500

    fix: button disabled styling cursor

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,7 +9,7 @@ import { cva } from 'class-variance-authority';
 import { cn } from '~/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 disabled:relative disabled:before:(content-[""] absolute top-0 left-0 w-full h-full cursor-not-allowed pointer-events-auto)',
   {
     variants: {
       variant: {
