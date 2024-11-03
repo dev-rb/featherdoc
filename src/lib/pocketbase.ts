00864dbc0ff9d1d6cc2a6c5521a25792075a6b5c
dev-rb <rbatra486@gmail.com>
2024-11-03 15:18:50 -0500

    fix: mutations error and formatting

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -15,29 +15,27 @@ type QueryNames = keyof CollectionRecords;
 type GetParams<Name extends QueryNames, Method extends QueryMethods | MutationMethods> = Method extends 'create'
   ? [values: CollectionRecords[Name], options?: RecordOptions]
   : Method extends 'update'
-  ? [id: string, values: Partial<CollectionRecords[Name]>, options?: RecordOptions]
-  : Name extends keyof CollectionResponses
-  ? Parameters<RecordService<CollectionRecords[Name]>[Method]>
-  : never;
+    ? [id: string, values: Partial<CollectionRecords[Name]>, options?: RecordOptions]
+    : Name extends keyof CollectionResponses
+      ? Parameters<RecordService<CollectionRecords[Name]>[Method]>
+      : never;
 
 type ToAccessors<T> = { [K in keyof T]: Accessor<T[K]> };
 
 type QueryOptions = Pick<ResourceOptions<unknown>, 'initialValue' | 'deferStream'> & { enabled?: Accessor<boolean> };
 
-type NarrowReturnType<T> = T extends Promise<infer A> ? A : T extends Promise<ListResult<infer B>> ? B : T
+type NarrowReturnType<T> = T extends Promise<infer A> ? A : T extends Promise<ListResult<infer B>> ? B : T;
 
-type CorrectValue<A, B> = A extends ListResult<any> ? ListResult<B> : B
-
+type CorrectValue<A, B> = A extends ListResult<any> ? ListResult<B> : B;
 
 type QueryReturn<Name extends QueryNames, Method extends QueryMethods> = {
-
-  data: InitializedResource<Awaited<
-    CorrectValue<NarrowReturnType<ReturnType<RecordService[Method]>>, CollectionResponses[Name]>
-  >>,
-  isLoading: Accessor<boolean>
-  isRefreshing: Accessor<boolean>
-  isFetching: Accessor<boolean>
-}
+  data: InitializedResource<
+    Awaited<CorrectValue<NarrowReturnType<ReturnType<RecordService[Method]>>, CollectionResponses[Name]>>
+  >;
+  isLoading: Accessor<boolean>;
+  isRefreshing: Accessor<boolean>;
+  isFetching: Accessor<boolean>;
+};
 
 function createQuery<Name extends QueryNames, Method extends QueryMethods>(
   name: Name,
@@ -47,46 +45,44 @@ function createQuery<Name extends QueryNames, Method extends QueryMethods>(
     options?: QueryOptions,
   ]
 ) {
-  const pb = usePocketbase()
+  const pb = usePocketbase();
   const getResourceOptions = () => {
-    const lastParam = params[params.length - 1]
+    const lastParam = params[params.length - 1];
 
     if (lastParam && ('deferStream' in lastParam || 'initialValue' in lastParam || 'enabled' in lastParam)) {
-      const { enabled, ...rest } = lastParam
-      return { resource: rest, enabled }
+      const { enabled, ...rest } = lastParam;
+      return { resource: rest, enabled };
     }
 
-    return
-  }
-  const options = getResourceOptions()
+    return;
+  };
+  const options = getResourceOptions();
   const [data, s] = createResource(
     createMemo(() => {
-      if (options?.enabled && !options.enabled()) return
-      const list =
-        params.map((c) => {
-          if (typeof c === 'function') return c();
-          return c;
-        })
+      if (options?.enabled && !options.enabled()) return;
+      const list = params.map((c) => {
+        if (typeof c === 'function') return c();
+        return c;
+      });
 
-      return list
-    }
-    ),
+      return list;
+    }),
     async (resolvedParams) => {
-      const event = getRequestEvent()
-      const _pb = (isServer ? event ? event?.locals.pb : pb : pb) as TypedPocketBase
+      const event = getRequestEvent();
+      const _pb = (isServer ? (event ? event?.locals.pb : pb) : pb) as TypedPocketBase;
       const result = (await _pb
         .collection<CollectionRecords[Name]>(name)
-      // @ts-expect-error no clue how to type this
-      [method]<CollectionResponses[Name]>(...resolvedParams)) as (Awaited<
+        // @ts-expect-error no clue how to type this
+        [method]<CollectionResponses[Name]>(...resolvedParams)) as Awaited<
         CorrectValue<NarrowReturnType<ReturnType<RecordService[Method]>>, CollectionResponses[Name]>
-      >);
+      >;
       console.debug(`[createQuery]: 
                    (token): ${_pb.authStore.token || 'EMPTY'} 
                    (method): ${method} 
                    (params): ${JSON.stringify(resolvedParams)} 
-                   (result): ${JSON.stringify(result)}`)
+                   (result): ${JSON.stringify(result)}`);
 
-      return result
+      return result;
     },
     { ...(options?.resource as any) }
   );
@@ -99,20 +95,21 @@ function createQuery<Name extends QueryNames, Method extends QueryMethods>(
     isFetching: () => data.state === 'pending',
     refetch: s.refetch,
   };
-};
+}
 
 const createMutation = <Name extends QueryNames, Method extends MutationMethods>(name: Name, method: Method) => {
-  const pb = usePocketbase()
-  const action = pb.collection(name)[method];
+  const pb = usePocketbase();
 
   return {
     mutate: (...params: GetParams<Name, Method>): void => {
-      action(params as any);
+      // @ts-expect-error Not sure how to fix rest param types
+      pb.collection(name)[method](...(params as any[]));
     },
     mutateAsync: async (
       ...params: GetParams<Name, Method>
     ): Promise<Awaited<ReturnType<RecordService<CollectionRecords[Name]>[Method]>>> => {
-      return (await action(params as any)) as any;
+      // @ts-expect-error Not sure how to fix rest param types
+      return (await pb.collection(name)[method](...(params as any))) as any;
     },
   };
 };
