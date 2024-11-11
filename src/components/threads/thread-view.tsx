d399aa804f69fecb37fb25f5047e124732ae48e5
dev-rb <me@devrb.dev>
2024-11-11 01:07:06 -0500

    fix: add remove button for attachments and update styling

--- src/components/threads/thread-view.tsx
+++ src/components/threads/thread-view.tsx
@@ -233,20 +233,34 @@ export const ThreadView: VoidComponent<ThreadViewProps> = (props) => {
                       when={comment.attachments.length > 1}
                       fallback={
                         <Show when={comment.attachments.length === 1}>
-                          <div class="w-fit bg-secondary cursor-zoom-in rounded-lg">
+                          <div class="group relative w-fit bg-secondary rounded-lg cursor-zoom-in">
                             <img
                               class="size-28 object-cover rounded-lg"
                               src={pb.files.getUrl(comment, comment.attachments[0])}
                             />
+                            <Button
+                              variant="destructive"
+                              size="icon"
+                              class="group-hover:flex hidden size-6 absolute top-0 right-0 rounded-full translate-x-1/2 -translate-y-1/2"
+                            >
+                              <i class="i-lucide-x inline-block" />
+                            </Button>
                           </div>
                         </Show>
                       }
                     >
-                      <div class="w-full flex flex-wrap gap-2">
+                      <div class="w-full flex flex-wrap gap-4">
                         <For each={comment.attachments}>
                           {(attachment) => (
-                            <div class="w-fit bg-secondary rounded-lg cursor-zoom-in">
+                            <div class="group relative w-fit bg-secondary rounded-lg cursor-zoom-in">
                               <img class="size-28 object-cover rounded-lg" src={pb.files.getUrl(comment, attachment)} />
+                              <Button
+                                variant="destructive"
+                                size="icon"
+                                class="group-hover:flex hidden size-6 absolute top-0 right-0 rounded-full translate-x-1/2 -translate-y-1/2"
+                              >
+                                <i class="i-lucide-x inline-block" />
+                              </Button>
                             </div>
                           )}
                         </For>
