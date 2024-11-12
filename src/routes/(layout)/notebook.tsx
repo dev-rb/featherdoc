dc8980645a3622c7d372b3141a517def23e7679e
dev-rb <me@devrb.dev>
2024-11-11 21:59:01 -0500

    feat: create notebook, debounce update, and saving indicator

--- src/routes/(layout)/notebook.tsx
+++ src/routes/(layout)/notebook.tsx
@@ -1,11 +1,12 @@
 import { Title } from '@solidjs/meta';
-import { ClientResponseError } from 'pocketbase';
+import { debounce } from '@solid-primitives/scheduled';
 import { createMemo, ErrorBoundary, Show } from 'solid-js';
 import { useApp } from '~/components/app-context';
 import { usePocketbase } from '~/components/pocketbase-context';
 import { Button } from '~/components/ui/Button';
 import { RichEditor } from '~/components/ui/rich-editor';
-import { createMutation, createQuery } from '~/lib/pocketbase';
+import { createMutation, createQuery, invalidateQuery } from '~/lib/pocketbase';
+import { cn } from '~/lib/utils';
 
 export default function Notebook() {
   const app = useApp();
@@ -18,7 +19,20 @@ export default function Notebook() {
     { enabled: () => app.session().userId !== undefined }
   );
 
+  const createNotebook = createMutation('notebooks', 'create', {
+    async onSuccess() {
+      await invalidateQuery('notebooks/getFirstListItem');
+    },
+  });
   const updateNotebook = createMutation('notebooks', 'update');
+
+  const handleCreate = () => {
+    const userId = app.session().userId;
+
+    if (!userId) return;
+
+    createNotebook.mutate({ author: userId });
+  };
 
   const handleUpdate = (value: string) => {
     const id = notebook.data()?.id;
@@ -26,11 +40,24 @@ export default function Notebook() {
     updateNotebook.mutate(id, { content: value });
   };
 
+  const debouncedUpdate = debounce((value: string) => {
+    handleUpdate(value);
+  }, 225);
+
   return (
     <>
       <Title>Notebook | Support World</Title>
       <div class="w-full h-full grid grid-rows-1 lg:grid-cols-[1fr_2fr_1fr] lg:p-4">
-        <div class="col-start-1 lg:col-start-2 w-full h-full bg-muted lg:rounded-lg p-4">
+        <div class="relative col-start-1 lg:col-start-2 w-full h-full bg-muted lg:rounded-lg p-4">
+          <div
+            class={cn(
+              'absolute top-2 right-2 text-primary flex items-center gap-2 opacity-0 transition-opacity',
+              updateNotebook.isPending && 'opacity-100'
+            )}
+          >
+            <i class="i-svg-spinners-bars-rotate-fade inline-block text-lg" />
+            Saving...
+          </div>
           <ErrorBoundary
             fallback={(e) => {
               const isNotFoundError = () => {
@@ -47,7 +74,7 @@ export default function Notebook() {
                     Notebook not found
                   </Show>
 
-                  <Button class="gap-2">
+                  <Button class="gap-2" loading={createNotebook.isPending} onClick={handleCreate}>
                     <i class="i-lucide-plus inline-block" />
                     Create one
                   </Button>
@@ -58,7 +85,7 @@ export default function Notebook() {
             <RichEditor
               class="w-full h-full focus:outline-none text-white"
               contents={notebook.data.latest?.content}
-              onInput={(value) => handleUpdate(value)}
+              onInput={(value) => debouncedUpdate(value)}
             />
           </ErrorBoundary>
         </div>
