4bef30e2f8a31a2b39252679114df0e03fad4a5b
dev-rb <rbatra486@gmail.com>
2024-11-14 19:53:34 -0500

    fix: add placeholder for dashboard

--- src/routes/(layout)/index.tsx
+++ src/routes/(layout)/index.tsx
@@ -4,13 +4,10 @@ export default function Home() {
   return (
     <main class="w-full h-full grid grid-cols-12 grid-rows-12 bg-background">
       <AuthModal />
-      <div class="w-full h-full col-start-2 row-start-2 col-end-6 row-end-6 rounded-md border-1 border-neutral-600"></div>
-
-      <div class="w-full h-full col-start-8 row-start-2 col-end-12 row-end-8 rounded-md border-1 border-neutral-600"></div>
-
-      <div class="w-full h-full col-start-2 row-start-7 col-end-6 row-end-12 rounded-md border-1 border-neutral-600"></div>
-
-      <div class="w-full h-full col-start-7 row-start-9 col-end-12 row-end-12 rounded-md border-1 border-neutral-600"></div>
+      <div class="place-self-center w-full h-full text-center bg-muted col-start-2 col-end-12 row-start-2 row-end-12 rounded-2xl text-center text-xl flex-center flex-col gap-4 text-primary">
+        ✍(◔◡◔)
+        <span>Dashboad coming soon</span>
+      </div>
     </main>
   );
 }
